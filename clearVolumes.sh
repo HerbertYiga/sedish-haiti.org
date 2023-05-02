956d323316c52935d729e14547b430defcdc8dbd
HerbertYiga <herbertyiga@gmail.com>
2023-05-02 23:00:14 +0300

    Clearing volumes

--- clearVolumes.sh
+++ clearVolumes.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 ###########################################
 #
-# Simple Shell script to clean/remove  opencr and  containers
+# Simple Shell script to clean/remove  opencr-fhir and  opencr containers
 #
 # The script will 
-#  - first stop the opencr and  containers 
-#  - remove opencr and  containers
-#  - remove opencr and  images
-#  - remove opencr and  volumes
+#  - first stop the opencr-fhir and opencr containers 
+#  - remove opencr-fhir containers
+#  - remove opencr
+#  - remove opencr images
 #
 
 # stop opencr container
 echo '####################################################'
-echo 'Stopping running opencr and  containers (if available)...'
+echo 'Stopping running opencr and opencr-fhir containers (if available)...'
 echo '####################################################'
-docker stop  opencr 
+docker stop  opencr opencr-fhir
+
+# remove  opencr-fhir stopped containers
+echo '####################################################'
+echo 'Removing opencr-fhir container ..'
+echo '####################################################'
+docker rm opencr-fhir
 
 # remove  opencr stopped containers
 echo '####################################################'
@@ -22,15 +28,14 @@ echo 'Removing opencr container ..'
 echo '####################################################'
 docker rm opencr 
 
-
-# remove  opencr and  images
+# remove  opencr images
 echo '####################################################'
-echo 'Removing  opencr and  images ...'
+echo 'Removing  opencr images ...'
 echo '####################################################'
-docker rmi opencr 
+docker rmi intrahealth/opencr:latest
 
 # remove  opencr stray volumes if any
-echo '####################################################'
+echo '##################################################'
 echo 'Revoming  opencr container volumes (if any)'
 echo '####################################################'
-docker volume rm opencr 
+docker volume rm --force opencr
